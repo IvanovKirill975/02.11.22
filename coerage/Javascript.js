1cb75f93d27e88815b2a2d69b5719a760d0289c6
IvanovKirill975 <kirill707.ivanov@gmail.com>
2022-11-02 20:35:24 +0200

    fff

---
+++ coerage/Javascript.js
@@ -0,0 +1,68 @@
+// const arr = [];
+// const arr1 = [1, 2, 3, 4,"salut", "tri",true];
+// console.log(arr);
+// console.log(arr1.length);
+
+// const user = prompt("Cum te numesti")
+// const user1 = prompt("Citi ani ai")
+
+// const userArray = []
+// const userArray1 = ["Ion"]
+
+// userArray.push(userArray1)
+// userArray.push(user)
+// userArray.push(user1)
+// console.log(userArray);
+
+// let obj = {
+//     name:"Vasile",
+//     age:"26",
+//     gen:"M",
+// }
+
+// console.log(obj);
+// console.log(obj.name, obj.age);
+
+// let obj_1 = {
+//     name:"Nina",
+//     age:"39",
+//     gen:"F",
+//      nameChild:{
+//         name:"Viorel",
+//         age:"5",
+//         geh:"M",
+//     }
+// }
+// console.log(typeof obj_1.nameChild.age );
+
+// const user = [
+//     { name: "Alex"},
+//      { name: "ion"},
+//      { name: "Alsdgsd"},
+//      { name: "Alsdfsdvsdf"},
+// ]
+
+
+// user.forEach(el => {
+// console.log(el.name);
+// })
+
+const array = [
+    "1",
+     "2",
+     "3",
+     "4",
+     "5",
+     "6"
+];
+    array.forEach(el => {
+
+     //  console.log(el.includes("6"));
+
+     if (el.includes("6")) {
+        console.log(el);
+     }else{
+        console.log("nui");
+     }
+
+    })
